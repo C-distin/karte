e3c9581a2d62bc81a7522702f444ff8a746adbf4
Tricky <patrickannang23@gmail.com>
2025-05-08 13:36:39 +0000

    fix fonts

--- components/fonts.ts
+++ components/fonts.ts
@@ -3,15 +3,12 @@ import { Pacifico, Playwrite_DK_Loopet, Lilita_One } from "next/font/google";
 export const pacifico = Pacifico({
   weight: "400",
   subsets: ["latin"],
-  display: "swap",
 })
 
 export const playwrite = Playwrite_DK_Loopet({
-  weight: ["400"],
-  display: "swap",
+  weight: "400",
 })
 
 export const lilita = Lilita_One({
-  weight: ["400"],
-  display: "swap",
+  weight: "400",
 })
