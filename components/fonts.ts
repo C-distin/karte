075a52e22c0529da1551e3da5fe13323349c5478
spacegod1 <kojo.johnson44@gmail.com>
2025-05-25 23:24:44 +0000

    fix merge conflicts on ui

--- components/fonts.ts
+++ components/fonts.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Pacifico, Whisper, Playwrite_DK_Loopet, Lilita_One, UnifrakturMaguntia, Montserrat } from "next/font/google";
-=======
-import { Pacifico, Playwrite_DK_Loopet, Lilita_One, UnifrakturMaguntia, Nanum_Myeongjo, Great_Vibes, Sacramento, Kaushan_Script } from "next/font/google";
->>>>>>> ebc62bb46e5a8d48af7cf4427c3716f3670ce696
 
 export const pacifico = Pacifico({
   weight: "400",
@@ -33,27 +29,4 @@ export const unifraktur = UnifrakturMaguntia({
   weight: "400",
   subsets: ["latin"],
   display: "swap",
-<<<<<<< HEAD
-=======
-})
-
-export const nanum = Nanum_Myeongjo({
-  weight: "400",
-  subsets: ["latin"],
-})
-
-export const great = Great_Vibes({
-  weight: "400",
-  subsets: ["latin"],
-})
-
-export const sacramento = Sacramento({
-  weight: "400",
-  subsets: ["latin"],
-})
-
-export const kaushan = Kaushan_Script({
-  weight: "400",
-  subsets: ["latin"],
->>>>>>> ebc62bb46e5a8d48af7cf4427c3716f3670ce696
 })
